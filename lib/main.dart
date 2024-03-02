f4dcc9fecd64df98944c8180aff1217b98cd1edf
Mehedi Hasan <mehedihasan62086@gmail.com>
2024-03-02 23:57:55 +0600

    feat(blocObserver): done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:bloc_api_project/bloc/products_bloc.dart';
+import 'package:bloc_api_project/debug/bloc_observer.dart';
 import 'package:bloc_api_project/repo/products_repo.dart';
 import 'package:bloc_api_project/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(RepositoryProvider(
     create: (context) => ProductsRepo(),
     child: const MyApp(),
