79a7173aa98995b5ca82de47343c321a189e62ea
Mehedi Hasan <mehedihasan62086@gmail.com>
2024-03-05 16:23:45 +0600

    feat(cubit): done

--- lib/screens/users_screen.dart
+++ lib/screens/users_screen.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_api_project/cubit/users_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class UsersScreen extends StatefulWidget {
   const UsersScreen({super.key});
@@ -9,6 +12,12 @@ class UsersScreen extends StatefulWidget {
 
 class _UsersScreenState extends State<UsersScreen> {
   @override
+  void initState() {
+    context.read<UsersCubit>().onLoadedUsers();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -17,10 +26,114 @@ class _UsersScreenState extends State<UsersScreen> {
           ),
           backgroundColor: Colors.indigo,
         ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return const Text("Users Name");
+        body: BlocConsumer<UsersCubit, UsersState>(
+          listener: (context, state) {
+            if (state is UsersLoadedState) {
+              VxToast.show(context,
+                  msg: "Velocity x Users Loaded",
+                  position: VxToastPosition.top);
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text("Data Loaded")));
+            } else if (state is UsersErrorState) {
+              VxToast.show(context,
+                  msg: "Velocity x Users Not Loaded",
+                  position: VxToastPosition.top);
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Data Not Loaded")));
+            }
+          },
+          builder: (context, state) {
+            return BlocBuilder<UsersCubit, UsersState>(
+              builder: (context, state) {
+                if (state is UsersLoadingState) {
+                  return const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+                } else if (state is UsersLoadedState) {
+                  return ListView.separated(
+                    separatorBuilder: (context, index) => const SizedBox(
+                      height: 8,
+                    ),
+                    itemCount: state.usersModel.length,
+                    itemBuilder: (context, index) {
+                      var data = state.usersModel[index];
+                      return Card(
+                        child: Column(
+                          children: [
+                            Text(
+                              data.id.toString(),
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.indigo),
+                            ),
+                            Text(
+                              data.name.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.username.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.email.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.address!.street.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.address!.suite.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.address!.city.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.address!.zipcode.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.address!.geo!.lat.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.address!.geo!.lng.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.phone.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.website.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.company!.name.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.company!.catchPhrase.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              data.company!.bs.toString(),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                } else if (state is UsersErrorState) {
+                  return Center(
+                    child: Text(state.errorMessage),
+                  );
+                }
+                return const SizedBox();
+              },
+            );
           },
         ));
   }
